501949f4b9b4ca32b7907456efeefc2e59129cc2
Grace Duncan <grace@spoonity.com>
2025-10-07 10:09:46 -0400

    Testing copilot & merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -159,7 +159,7 @@ export default function App() {
             </div>
           ) : !showPreview ? (
             <>
-              <h1>Create new promotion content</h1>
+              <h1>This is a header!</h1>
               <p className="page-description">
                 Generate promotional content based on competitor's promotions in your area
               </p>
